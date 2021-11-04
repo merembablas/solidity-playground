5981c6899696605a80d0a9cbe5afa90d9bd06bba
Rifqi Alfian <merembablas@gmail.com>
2021-11-04 14:26:54 +0700

    feat(contract): add burnable, capped, and pausable token

--- contracts/LOLToken.sol
+++ contracts/LOLToken.sol
@@ -1,14 +1,37 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LOLToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Lololo", "LOL") {
-        _mint(msg.sender, initialSupply);
+contract LOLToken is ERC20Capped, ERC20Burnable, ERC20Pausable, Ownable {
+    constructor(uint256 initialSupply) ERC20('Lolololo', 'LOL') ERC20Capped(10000) {
+        ERC20._mint(msg.sender, initialSupply);
     }
 
     function decimals() public view virtual override returns (uint8) {
         return 0;
+    }
+
+    function _mint(address account, uint256 amount) internal virtual override(ERC20, ERC20Capped) {
+        ERC20Capped._mint(account, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Pausable) {
+        ERC20Pausable._beforeTokenTransfer(from, to, amount);
+    }
+
+    function mint(uint256 amount) external onlyOwner {
+        _mint(msg.sender, amount);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
